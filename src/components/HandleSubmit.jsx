feb2c8e472b2f034b01818605967092ae56a359c
Andreas Laine <95121638+Messijahz@users.noreply.github.com>
2024-10-30 19:13:55 +0100

    Added regex to the email form on the bottom of the main page.

--- src/components/HandleSubmit.jsx
+++ src/components/HandleSubmit.jsx
@@ -5,6 +5,8 @@ const EmailForm = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [confirmationMessage, setConfirmationMessage] = useState("");
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
   const handleChange = (e) => {
     setEmail(e.target.value);
   };
@@ -13,15 +15,14 @@ const EmailForm = () => {
     e.preventDefault(); // Gör så att sidan inte laddas om direkt innan vi kontrollerat om epostadressen är giltig.
 
     
-    if (email.length >= 5 && email.includes("@") && email.includes(".")) { // Vi validerar om användaren skrivit in minst 5 tecken i fältet. Detta måste innehålla ett @-tecken och ett tecken måste vara en punkt.
-      setErrorMessage(""); // Tar bort felmeddelandet om e-postadressen skulle vara giltig.
-      setConfirmationMessage("Thank you for signing up!");
-      setEmail("");
+    if (emailRegex.test(email)) {
+      setErrorMessage(""); // Rensa fältet på ett eventuellt felmeddelande om epostadressen är valid.
+      setConfirmationMessage("* Thank you for signing up!");
+      setEmail(""); // Tömmer inputfältet.
     } else {
-      setErrorMessage("* Email is not valid."); // Visar vårat felmeddelande om adressen är ogiltig.
-      setConfirmationMessage("");
+      setErrorMessage("* Email is not valid."); // Visar felmeddelande om att emailen ej är valid.
+      setConfirmationMessage(""); // Rensar fältet om eposten är ogiltig. 
     }
-
   };
 
   return (
@@ -43,8 +44,8 @@ const EmailForm = () => {
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
                 {confirmationMessage && <p className="confirmation-message">{confirmationMessage}</p>}
             </div>
-        <button className="contact-btn-subscribe"type="submit">
-        Subscribe
+        <button className="contact-btn-subscribe" type="submit">
+          Subscribe
         </button>
         </form>
     </div>
